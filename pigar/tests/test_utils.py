7d8902eea58ecaea321f96daf51a4c7f3144146c
Damnever <dxc_wolf@163.com>
2015-10-06 10:33:06 +0800

    compare version testcases

--- pigar/tests/test_utils.py
+++ pigar/tests/test_utils.py
@@ -4,9 +4,12 @@ from __future__ import print_function, division, absolute_import
 
 import unittest
 import os
+import random
 
 from .helper import CaptureOutput
-from ..utils import Dict, print_table, parse_reqs
+from ..utils import (
+    Dict, print_table, parse_reqs, compare_version, cmp_to_key
+)
 
 
 class DictTests(unittest.TestCase):
@@ -71,7 +74,7 @@ class PrintTableTests(unittest.TestCase):
         self.assertListEqual(output, target)
 
 
-class ParseReqsTests(unittest.TestCase):
+class ParseReqsTest(unittest.TestCase):
 
     def test_parse_reqs(self):
         path = os.path.join(os.path.dirname(__file__),
@@ -79,3 +82,23 @@ class ParseReqsTests(unittest.TestCase):
         target = {'a': '4.1.4', 'b': '2.3.0', 'c': ''}
         reqs = parse_reqs(path)
         self.assertDictEqual(reqs, target)
+
+
+class CompareVersionTests(unittest.TestCase):
+
+    def test_compare_version(self):
+        self.assertEqual(compare_version('1.1.1', '1.2.1'), -1)
+        self.assertEqual(compare_version('1.10.1', '1.2.1'), 1)
+        self.assertEqual(compare_version('1.2', '1.2.1'), -1)
+        self.assertEqual(compare_version('1.1.1', '1.1.1'), 0)
+        self.assertEqual(compare_version('1.1.1', '1.1.1b'), -1)
+        self.assertEqual(compare_version('1.1b2', '1.1a2'), 1)
+        self.assertEqual(compare_version('1.1b1', '1.1b2'), -1)
+        self.assertEqual(compare_version('1.1b', '1.1b'), 0)
+
+    def test_sort_versions(self):
+        target = ['1.1.1', '1.1.1b1', '1.1.1b2', '1.2.1', '1.10.1']
+        test = target[:]
+        random.shuffle(test)
+        self.assertListEqual(sorted(test, key=cmp_to_key(compare_version)),
+                             target)
