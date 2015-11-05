3257f1410df202a5654db309c53c1862e648ecdd
Damnever <dxc_wolf@163.com>
2015-11-05 12:53:10 +0800

    new version

--- pigar/_version.py
+++ pigar/_version.py
@@ -2,5 +2,5 @@
 
 from __future__ import print_function, division, absolute_import
 
-version = __version__ = '0.6.1'
+version = __version__ = '0.6.2'
 version_info = [int(num) for num in version.split('.')]
