8454555a5f269336b15255b3bddd6c5f004bec33
Taranjeet <reachtotj@gmail.com>
2015-11-08 18:49:47 +0530

    fixes the pep8 warnings

--- pigar/reqs.py
+++ pigar/reqs.py
@@ -101,12 +101,12 @@ class ImportChecker(object):
         """
         if hasattr(node.body, 's'):
             self._str_codes.append(
-                    (getattr(node.body, 's'), node.lineno + self._lineno))
+                (getattr(node.body, 's'), node.lineno + self._lineno))
         # Sometimes exec statement may be called with tuple in py2
         elif hasattr(node.body, 'elts') and len(node.body.elts) >= 1:
             self._str_codes.append(
-                    (getattr(node.body.elts[0], 's'),
-                     node.lineno + self._lineno))
+                (getattr(node.body.elts[0], 's'),
+                    node.lineno + self._lineno))
 
     def visit_Expr(self, node):
         """
