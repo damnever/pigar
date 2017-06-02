5df84d580b000ee8dc5736c7941c369bf59eab1a
damnever <dxc.wolf@gmail.com>
2017-06-02 23:37:19 +0800

    Bump version

--- pigar/_version.py
+++ pigar/_version.py
@@ -2,5 +2,5 @@
 
 from __future__ import print_function, division, absolute_import
 
-version = __version__ = '0.6.10'
+version = __version__ = '0.7.0'
 version_info = [int(num) for num in version.split('.')]
