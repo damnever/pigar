be21189afa201634ba769665c20037f9584e517b
Xiaochao Dong (@damnever) <the.xcdong@gmail.com>
2022-11-07 17:05:08 +0800

    Minor change & add some test cases
    Signed-off-by: Xiaochao Dong (@damnever) <the.xcdong@gmail.com>

--- pigar/parser.py
+++ pigar/parser.py
@@ -56,12 +56,15 @@ def parse_imports(package_root, ignores=None):
     return imported_modules, user_modules
 
 
-def _filter_ipynb_commands(cell_lines):
-    """
-    Filter out cells with magics and shell commands
-    e.g %matplotlib inline or !pip install pigar
-    """
-    return re.sub(r"^((\s)*(!|%).*$)|(\n\s*%.*)", "", cell_lines)
+# Match ipython notebook magics and shell commands.
+# e.g %matplotlib inline or !pip install pigar
+#
+# Ref:
+#  - https://ipython.readthedocs.io/en/stable/interactive/magics.html
+#  - https://ipython.org/ipython-doc/3/interactive/shell.html
+_ipynb_magics_and_commands_regex = re.compile(
+    r"[^#]*\s*(!|%)[a-zA-Z][a-zA-Z0-9_-]*.*"
+)
 
 
 def _read_code(fpath):
@@ -69,9 +72,13 @@ def _read_code(fpath):
         nb = nbformat.read(fpath, as_version=4)
         code = ""
         for cell in nb.cells:
-            if cell.cell_type == "code":
-                source = _filter_ipynb_commands(cell.source)
-                code += source + "\n"
+            if cell.cell_type != "code":
+                continue
+            for line in cell.source.splitlines():
+                match = _ipynb_magics_and_commands_regex.match(line)
+                if not (match and match.group(0) == line):
+                    code += line
+                code += "\n"
         return code
     elif fpath.endswith(".py"):
         with open(fpath, 'rb') as f:
@@ -96,6 +103,7 @@ def parse_file_imports(fpath, content):
 
 
 class ImportsParser(object):
+
     def __init__(self, rawcode_callback=None):
         self._modules = []
         self._rawcode_callback = rawcode_callback
@@ -308,7 +316,7 @@ def _search_path(path):
                     line = line.strip()
                     if line != '.':
                         dev_dir = line
-            if not dev_dir or not pathlib.exists(dev_dir):
+            if not dev_dir:
                 continue
             # Egg info path.
             info_dir = [
