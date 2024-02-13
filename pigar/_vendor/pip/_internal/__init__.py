35a9c0d00a0f56027b1b4a5dba1622bee874fb9b
Xiaochao Dong (@damnever) <the.xcdong@gmail.com>
2024-02-13 16:01:42 +0800

    Update pip to 24.0
    Signed-off-by: Xiaochao Dong (@damnever) <the.xcdong@gmail.com>

--- pigar/_vendor/pip/_internal/__init__.py
+++ pigar/_vendor/pip/_internal/__init__.py
@@ -1,6 +1,5 @@
 from typing import List, Optional
 
-import pigar._vendor.pip._internal.utils.inject_securetransport  # noqa
 from pigar._vendor.pip._internal.utils import _log
 
 # init_logging() must be called before any call to logging.getLogger()
